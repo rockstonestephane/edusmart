20d545f357e8ef1f7f7da7e4a7f77a528cc7b1d2
Franck Stéphane MBONO MBONO <franckrockstone@icloud.com>
2026-03-08 13:09:03 +0100

    fix: force HTTPS en production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,19 +1,21 @@
 <?php
-
 namespace App\Providers;
-
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\View;
 use App\Http\View\Composers\SchoolComposer;
 use Illuminate\Support\Facades\Schema;
-
 class AppServiceProvider extends ServiceProvider
 {
     public function register(): void {}
-
     public function boot(): void
     {
         Schema::defaultStringLength(191);
+        
+        // Force HTTPS en production
+        if (config('app.env') === 'production') {
+            \URL::forceScheme('https');
+        }
+        
         // Partage les variables school sur toutes les vues frontend
         View::composer([
             'frontend.components.navbar',
